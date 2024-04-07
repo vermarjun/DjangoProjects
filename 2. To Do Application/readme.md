9df13809cf4dbded393c08ba50af342e7b9d3d46
Arjun Verma <vermarjun26@gmail.com>
2024-04-07 13:03:05 +0530

    Delete 2. To Do Application directory

--- 2. To Do Application/readme.md
+++
@@ -1,2 +0,0 @@
-# Simple To Do WebApp
-Making this helped me understand how to manage elements and delete when clicked
--- 2. To Do Application/todo/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms 
-from .models import ToDo
-
-class ToDoForm(forms.ModelForm):
-    class Meta:
-        model = ToDo
-        fields = "__all__"      
